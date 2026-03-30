1ef767a8363ad2dca74f2a3d7eadb2452d6d7fad
Eques72 <adrian.zareba842@proton.me>
2026-03-30 13:24:53 +0200

    contact done for the most part

--- static/js/submit.js
+++ static/js/submit.js
@@ -4,3 +4,21 @@ document.querySelector('#contact-form').addEventListener('submit', (e) => {
     e.target.elements.email.value = '';
     e.target.elements.message.value = '';
   });
+
+const a = document.querySelector('.direct-contact-container');
+const b = document.querySelector('.form-horizontal');
+
+function equalizeHeights(a, b) {
+  if (!a || !b) return;
+  if (window.matchMedia('(max-width: 1300px)').matches) {
+    a.style.height = '';
+    b.style.height = '';
+  } else {
+    const maxHeight = Math.max(a.offsetHeight, b.offsetHeight);
+    a.style.height = maxHeight + 'px';
+    b.style.height = maxHeight + 'px';
+  }
+}
+
+equalizeHeights(a,b);
+window.addEventListener('resize', () => equalizeHeights(a, b));
