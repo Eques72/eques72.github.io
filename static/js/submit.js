8277cf339117113cfeb1a43303c0c4c628ac93b3
Eques72 <adrian.zareba842@proton.me>
2026-03-30 18:00:55 +0200

    contact email sumbission

--- static/js/submit.js
+++ static/js/submit.js
@@ -1,10 +1,39 @@
-document.querySelector('#contact-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    e.target.elements.subject.value = '';
-    e.target.elements.email.value = '';
-    e.target.elements.message.value = '';
-  });
+function showTooltip(message, type) {
+    const tooltip = document.getElementById('form-tooltip');
+    tooltip.textContent = message;
+    tooltip.className = `tooltip visible ${type}`;
+    setTimeout(() => tooltip.className = 'tooltip', 5000);
+}
 
+emailjs.init('lqui25lcE43YDRMSp');
+
+window.onload = function() {
+  document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const btn = document.getElementById('submit');
+    btn.disabled = true;
+    
+    const templateMessage = {
+      name: e.target.elements.email.value.slice(0, e.target.elements.email.value.indexOf('@')),
+      email: e.target.elements.email.value,
+      title: e.target.elements.subject.value,
+      message: e.target.elements.message.value,
+    }
+
+    emailjs.send('service_5cwfx0t', 'template_hlhc6xk', templateMessage)
+      .then(() => {
+        showTooltip('Message sent.', 'success');
+        e.target.reset();
+      })
+      .catch(() => {
+          showTooltip('Failed to send.', 'error');
+      })
+      .finally(() => {
+          btn.disabled = false;
+      });
+    });
+}
 const a = document.querySelector('.direct-contact-container');
 const b = document.querySelector('.form-horizontal');
 
