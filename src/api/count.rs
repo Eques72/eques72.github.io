aa2abacf08619fbf5c57b7aa8afa7a90d908bb99
Eques72 <adrian.zareba842@proton.me>
2026-05-15 11:59:41 +0200

    removed local backend

--- src/api/count.rs
+++
@@ -1,9 +0,0 @@
-use axum::{routing::get, Json};
-
-use crate::services::counter::get_visitor_count;
-
-pub async fn get_count() -> Json<u64> {
-    let count = get_visitor_count();
-    Json(count as u64)
-}
-
--- src/state/app_state.rs
+++
@@ -1,6 +0,0 @@
-use tokio::sync::broadcast;
-
-#[derive(Clone)]
-pub struct AppState {
-    pub tx: broadcast::Sender<u64>,
-}
